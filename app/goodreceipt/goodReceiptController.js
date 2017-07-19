2e05c16948fae17df02388d1b05c070f7a60a344
Justine Lim Yu <jlyu@addu.edu.ph>
2017-07-19 23:25:51 +0800

    Turnover table logic and search modal (dummy)

--- app/goodreceipt/goodReceiptController.js
+++ app/goodreceipt/goodReceiptController.js
@@ -22,150 +22,95 @@
 
         //Dummy Item-List
         vm.itemList = [
-        				{
-        				 itemCode: 'Item-0001',
-        				 itemName: 'Item-Name1',
-        				 specification: 'Item-0001-Specification',
-        				 uom: 'Weight',
-        				 unitPrice: '100.00',
-        				 quantity: 50
-        				},
-        				{
-        				 itemCode: 'Item-0002',
-        				 itemName: 'Item-Name2',
-        				 specification: 'Item-0002-Specification',
-        				 uom: 'Kilogram',
-        				 unitPrice: '1200.00',
-        				 quantity: 25
-        				},
-        				{
-        				 itemCode: 'Item-0003',
-        				 itemName: 'Item-Name3',
-        				 specification: 'Item-0003-Specification',
-        				 uom: 'Weight',
-        				 unitPrice: '1700.00',
-        				 quantity: 5
-        				},
-        				{
-        				 itemCode: 'Item-0004',
-        				 itemName: 'Item-Name4',
-        				 specification: 'Item-Name4-Specification',
-        				 uom: 'Weight',
-        				 unitPrice: '12200.00',
-        				 quantity: 25
-        				}
-       				  ];
+			{
+				itemCode: 'Item-0001',
+				itemName: 'Item-Name1',
+				specification: 'Item-0001-Specification',
+				uom: 'Weight',
+				unitPrice: '100.00',
+				quantity: 50
+			},
+			{
+				itemCode: 'Item-0002',
+				itemName: 'Item-Name2',
+				specification: 'Item-0002-Specification',
+				uom: 'Kilogram',
+				unitPrice: '1200.00',
+				quantity: 25
+			},
+			{
+				itemCode: 'Item-0003',
+				itemName: 'Item-Name3',
+				specification: 'Item-0003-Specification',
+				uom: 'Weight',
+				unitPrice: '1700.00',
+				quantity: 5
+			},
+			{
+				itemCode: 'Item-0004',
+				itemName: 'Item-Name4',
+				specification: 'Item-Name4-Specification',
+				uom: 'Weight',
+				unitPrice: '12200.00',
+				quantity: 25
+			}
+		];
 
         //Copy From function using Dummy Data
         vm.copyFrom = function() {
         	vm.grTable = [];
         	vm.rowCount = 1;
-	        grDataTable.RowNo = vm.rowCount;
-	        var itemData = vm.itemList[0]; 
-	        grDataTable.itemData = itemData;   
-	        grDataTable.PlaceOfDelivery = 'Test Place of Delivery1';
-	        grDataTable.AssetNo = 'Asset-0001';
-	        grDataTable.SerialNo = 'Serial-0001';
+			var row1 = {
+				RowNo: vm.rowCount,
+				itemData: vm.itemList[0],
+				PlaceOfDelivery: 'Test Place of Delivery1',
+				AssetNo: 'Asset-0001',
+				SerialNo: 'Serial-0001'
+			};
+
+	        vm.grTable.push(row1);
+	        vm.rowCount++; //Sample number of count copied from...
+			var row2 = {
+				RowNo: vm.rowCount,
+				itemData: vm.itemList[1],
+				PlaceOfDelivery: 'Test Place of Delivery2',
+				AssetNo: 'Asset-0002',
+				SerialNo: 'Serial-0002'
+			};
 
 	        vm.grTable.push(grDataTable);
-	        vm.rowCount++ //Sample number of count copied from...
-
-	        grDataTable = {};
-	        grDataTable.RowNo = vm.rowCount;
-	        var itemData = vm.itemList[1]; 
-
-	        grDataTable.itemData = itemData;   
-	        grDataTable.PlaceOfDelivery = 'Test Place of Delivery2';
-	        grDataTable.AssetNo = 'Asset-0002';
-	        grDataTable.SerialNo = 'Serial-0002';
-
-	        vm.grTable.push(grDataTable);
-	        vm.rowCount++ //Sample number of count copied from...
+	        vm.rowCount++; //Sample number of count copied from...
         }
 
         //Adding Row function
         vm.addRow = function(){
-			grDataTable = {};
-        	grDataTable.RowNo = vm.rowCount;
         	var itemData = vm.itemList[0]; 
-
-        	grDataTable.itemData = itemData;   
-	        grDataTable.PlaceOfDelivery = '';
-	        grDataTable.AssetNo = '';
-	        grDataTable.SerialNo = '';
-
-	        vm.grTable.push(grDataTable);
-	        vm.rowCount++ //incrementing row count...
+	        vm.grTable.push({ itemData: itemData, RowNo: vm.rowCount });
+	        vm.rowCount++; //incrementing row count...
         }
 
         //Selecting Rows by clicking Row Number
         vm.selectIndex = function(rowNo){
-        	var rows = $('tr');
-        	vm.oldIndex = angular.isUndefined(vm.oldIndex) || vm.oldIndex == null ? 0 : vm.oldIndex;
-        	
-        	if(rowNo+1 == vm.oldIndex){
-        		rows.eq(vm.oldIndex).css("background-color", "");
-
-        		vm.indexSelected = null;
-        		vm.oldIndex = null;
-
-        		disableButton();
-        	}
-        	else{
-	        	rows.eq(vm.oldIndex).css("background-color", "");
-	        	rows.eq(rowNo+1).css("background-color", "darkgrey");
-
-	        	vm.oldIndex = rowNo + 1;
-	        	vm.indexSelected = rowNo;
-
-        		enableButton();
-        	}
+        	vm.selected = rowNo;
         }
 
         //Deleting Row if there's already selected
         vm.deleteRow = function(){
-        	var rows = $('tr');
-        	var index = vm.grTable.indexOf(vm.indexSelected);
-        	grDataTable = angular.copy(vm.grTable[vm.indexSelected]);
-
-        	rows.eq(vm.indexSelected+1).remove();
-
-        	angular.forEach(vm.grTable, function (value, key) {
-                if (value.RowNo == grDataTable.RowNo) {
-                   vm.grTable.splice(value.RowNo-1, 1);
-                }
-            });
-
-  			disableButton();
+			if(vm.selected >= 0) {
+        		vm.grTable.splice(vm.selected, 1);
+				vm.selected = null;
+			}
         }
 
         //Duplicating Row if there's already selected
         vm.duplicateRow = function(){
-        	var rows = $('tr');
-
-        	rows.eq(vm.indexSelected+1).css("background-color", "");
-        	grDataTable = {}
-        	grDataTable = angular.copy(vm.grTable[vm.indexSelected]);
-        	grDataTable.RowNo = vm.rowCount;
-
-        	vm.grTable.push(grDataTable);
-        	vm.rowCount++
-        	vm.indexSelected = null;
-    		vm.oldIndex = null;
-
-    		disableButton();
-        }
-
-        //Disabling Delete and Duplicate button
-        function disableButton(){
-        	vm.deleteBtn = true;
-        	vm.duplicateBtn = true;
-        }
-
-        //Enabling Delete and Duplicate button
-        function enableButton(){
-        	vm.deleteBtn = false;
-        	vm.duplicateBtn = false;
+			if(vm.selected >= 0){
+				var newRow = {};
+				angular.merge(newRow, vm.grTable[vm.selected]);
+				newRow.RowNo = vm.rowCount;
+				vm.grTable.push(newRow);
+				vm.rowCount++;
+			}
         }
     }
 })();
--- app/turnover/turnoverController.js
+++ app/turnover/turnoverController.js
@@ -9,7 +9,14 @@
         var vm = this;
         
         vm.init = function(){
-
+            vm.table = [];
+            vm.items = [{
+                itemCode: "Item-Code 001",
+                itemName: "Item-Name 1",
+                specification: "Spec 1",
+                uom: "Kilograms",
+                unitPrice: 500
+            }];
 
         }();
 
@@ -21,6 +28,40 @@
                 scope: $scope,
                 backdrop: 'static'
             });
+
+            $scope.ok = function(){ $scope.modalInstance.dismiss(); }
+            $scope.cancel = function(){ $scope.modalInstance.dismiss(); }
+        }
+
+        //Adding Row function
+        vm.addRow = function(){
+        	var itemData = vm.items[0]; 
+	        vm.table.push({ itemData: itemData, RowNo: vm.rowCount });
+	        vm.rowCount++; //incrementing row count...
+        }
+
+        //Selecting Rows by clicking Row Number
+        vm.selectIndex = function(rowNo){
+        	vm.selected = rowNo;
+        }
+
+        //Deleting Row if there's already selected
+        vm.deleteRow = function(){
+			if(vm.selected >= 0) {
+        		vm.table.splice(vm.selected, 1);
+				vm.selected = null;
+			}
+        }
+
+        //Duplicating Row if there's already selected
+        vm.duplicateRow = function(){
+			if(vm.selected >= 0){
+				var newRow = {};
+				angular.merge(newRow, vm.table[vm.selected]);
+				newRow.RowNo = vm.rowCount;
+				vm.table.push(newRow);
+				vm.rowCount++;
+			}
         }
     }
 })();
